b616a3bb39a2b50387adda7a0410b4fa42d4481a
diogogarbintrabalho-hue <diogogarbin.trabalho@gmail.com>
2026-08-01 00:32:28 +0000

    Deploy Segunda app

--- apps/node_app/index.js
+++ apps/node_app/index.js
@@ -2,9 +2,21 @@ const http = require('http');
 
 const PORT = 4000;
 
+// Variável para alternar entre os textos
+let alternar = false;
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('Aplicação Node.js rodando na EC2 - Resposta em texto simples.\n');
+    
+    // Alterna o estado a cada requisição que CHEGA ao Node.js
+    alternar = !alternar;
+    
+    const textoA = "Texto 1: Primeira versão da mensagem em texto simples.";
+    const textoB = "Texto 2: Segunda versão da mensagem após a expiração do cache!";
+    
+    const mensagemExibida = alternar ? textoA : textoB;
+    
+    res.end(`${mensagemExibida}\n`);
 });
 
 server.listen(PORT, '127.0.0.1', () => {
